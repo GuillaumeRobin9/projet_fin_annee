da843429267a4b6733fc1ecd1743ad3d8cc6bd22
Mickaeln44 <mickael.neroda@gmail.com>
2021-06-25 22:36:39 +0200

    :sparkles: creation/functions node Trie

--- structures_headers/structNodeTrie.h
+++ structures_headers/structNodeTrie.h
@@ -1,6 +1,3 @@
-//
-// Created by micka on 25/06/2021.
-//
 
 #ifndef CODE_STRUCTNODETRIE_H
 #define CODE_STRUCTNODETRIE_H
@@ -10,9 +7,34 @@
 #define MAX_LETTERS 26
 
 struct NodeTrie{
-    bool end;
+    bool isWord;
     int numberBirths;
-    struct NodeTrie* tab[MAX_LETTERS];
+    struct NodeTrie* letters[MAX_LETTERS];
 };
+
+
+//** Creation Function **
+struct NodeTrie* createEmptyNodeTrie();
+
+//** Suppression Function **
+void deleteNodeTrie(struct NodeTrie* trie);
+
+//** Access Functions **
+struct NodeTrie** getLetters(struct NodeTrie* trie);
+bool getIsWord(struct NodeTrie* trie);
+void addLetter(struct NodeTrie* trie, char c);
+void setIsWord(struct NodeTrie* trie, bool newBool);
+
+void insertWord(struct NodeTrie* trie, char* word);
+void deleteWord(struct NodeTrie* trie, char* word);
+bool belongs(struct NodeTrie* trie, char* word);
+bool isEmptyTrie(struct NodeTrie* trie);
+int numberOfWords(struct NodeTrie* trie);
+char* findWord(struct NodeTrie* trie, char* written);
+
+char posToChar(int pos);
+int charToPos(char c);
+
+
 
 #endif //CODE_STRUCTNODETRIE_H
