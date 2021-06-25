74f616a1288467ead1929b7cc4e3fe176e1bc462
Mickaeln44 <mickael.neroda@gmail.com>
2021-06-25 22:03:16 +0200

    :rocket: struct dataBase creation

--- structures_fonctions/structDataBase.c
+++ structures_fonctions/structDataBase.c
@@ -1,14 +1,75 @@
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
+
 #include "structures_headers/structDataBase.h"
+#include "structures_headers/structPerson.h"
+#include "structures_headers/structNodeTrie.h"
 
+struct dataBase* createEmptyDataBase(int numberPerson) {
 
-struct DataBase* CreateEmptyDataBase(){
+    struct dataBase* base = malloc(sizeof(struct dataBase));
 
+    if (base == NULL) {
+        return NULL;
+    }
 
+    base->personArray = malloc(numberPerson * sizeof (struct Person*));
+    if (base->personArray == NULL){
+        return NULL;
+    }
 
+    base->numberPerson = numberPerson;
+    for (int i = 0; i < 12; i++){
+        for (int j = 0; j < 31; j++){
+            base->birthdays[i][j] = 0;
+        }
+    }
+    base->oldestBirth = 0;
+    base->earliestBirth = 999999999;
+    base->IDoldestPerson = 0;
+    base->IDyoungestPerson = 0;
 
+    //** régions **
+    base->trie = createEmptyNodeTrie();
+    base->maxBirths = 0;
 
+}
+
+int getDayOfBirth(int day, int month, struct dataBase* data){
+    return data->birthdays[month-1][day-1];
+}
+
+int getAllPerson(struct dataBase* data){
+    return data->numberPerson;
+}
+
+int getOldestBirth(struct dataBase* data){
+    return data->oldestBirth;
+}
+
+int getEarliestBirth(struct dataBase* data){
+    return data->earliestBirth;
+}
+
+int getOldestID(struct dataBase* data){
+    return data->IDoldestPerson;
+}
+
+int getyoungestID(struct dataBase* data){
+    return data->IDyoungestPerson;
+}
+
+int getMaxBirths(struct dataBase* data){
+    return data->maxBirths;
+}
+
+struct NodeTrie* getTrie(struct dataBase* data){
+    return data->trie;
+}
+
+char* getFertileRegion(struct dataBase* data){
+    return data->fertileRegion;
 }
--- structures_headers/structDataBase.h
+++ structures_headers/structDataBase.h
@@ -1,28 +1,37 @@
-//
-// Created by micka on 25/06/2021.
-//
 
 #ifndef CODE_STRUCTDATABASE_H
 #define CODE_STRUCTDATABASE_H
 
-#define MAX_PERSON 10000 // utiliser plutot la valeur de la première ligne de la base de donnée
+#include "structNodeTrie.h"
 
 struct dataBase {
-    struct Person* personArray[MAX_PERSON];
+    struct Person** personArray;
     int numberPerson;
 
     int birthdays[12][31];
     int oldestBirth;
     int earliestBirth;
-    int IDoldestPerson; //calcul nombre de jour puis comparaison
+    int IDoldestPerson; //** calcul nombre de jour puis comparaison **
     int IDyoungestPerson;
 
-    // régions
+    //** régions **
     struct NodeTrie* trie;
     int maxBirths;
     char* fertileRegion;
 }
 
-struct dataBase* createEmptyDataBase();
+struct dataBase* createEmptyDataBase(int numberPerson);
+
+//** Access Function **
+
+int getAllPerson(struct dataBase* data);
+int getOldestBirth(struct dataBase* data);
+int getEarliestBirth(struct dataBase* data);
+int getOldestID(struct dataBase* data);
+int getyoungestID(struct dataBase* data);
+int getMaxBirths(struct dataBase* data);
+int getDayOfBirth(int day, int month, struct dataBase* data);
+struct NodeTrie* getTrie(struct dataBase* data);
+char* getFertileRegion(struct dataBase* data);
 
 #endif //CODE_STRUCTDATABASE_H
