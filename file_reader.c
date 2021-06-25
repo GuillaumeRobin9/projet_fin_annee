08b7e963c96f6207192630da673cef33ee04b58b
Guillaume ROBIN <71320274+GuillaumeRobin9@users.noreply.github.com>
2021-06-25 17:18:38 +0200

    Add files via upload

--- file_reader.c
+++ file_reader.c
@@ -1,60 +1,76 @@
-#include <stdlib>
+#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include "file_reader.h"
+//#include <bd_petite.csv>
 
-void read(int row, int col, char *nameFile,){
+#define MAX_CHAR 1000
 
-    FILE *file;
-    database = fopen(nameFile,"bd_petite");
-    if(database == NULL){
+
+void read(char *nameFile){
+
+    char row[MAX_CHAR];
+    char *token;
+    char* separators = ",/";
+    int l;
+    int index = 0;
+
+    //fill database ->createEmpty
+    FILE *file = fopen(nameFile,"r");
+
+    if(file == NULL){
         printf("Error in opening file");
-        return;
+        return NULL;
     }
-    char data[40];
-    int row = 0;
-    int column = 0;
-    while (fgets(data,40,file)) {
-        column = 0;
-        row++;
-        if (row == 1){
-            continue;
+
+    //char data[40];
+    char ID[25];
+    char IDf[25];
+    char IDm[25];
+    char lastName[25];
+    char firstName[25];
+    char day[25];
+    char month[25];
+    char year[25];
+    char region[25];
+
+    while (feof(file) != true) { //sauf 1er l
+
+        fgets(row, MAX_CHAR, file);
+        //printf("Row: %s", row);
+
+        //char **buffer = malloc(9 * sizeof(char *));
+
+        token = strtok(row, separators);
+
+        // first buffer use
+        l = strlen(token);
+        buffer[index] = malloc((l + 1) * sizeof(char));
+        buffer[index][l] = '\0';
+        //strcpy(buffer[index], token);
+        index++;
+
+        while (token != NULL) {  //not while
+            printf("Token: %s   ", token);
+            printf("%d",strlen(token));
+            // other buffer uses
+            l = strlen(token);
+            buffer[index] = malloc((l + 1) * sizeof(char));
+            buffer[index][l] = '\0';
+            //strcpy(buffer[index], token);
+            index++;
+
+            token = strtok(NULL, separators);
+
         }
-        char* value = strtok(data, ", "); //split the chain between the coma
+        printf("\n");
+        /*for (int i = 0; i < 9; i++) {
+            printf("¨buffer %d : %s\n", i, buffer[i]);
+        }*/
 
-        while (value) {
-            switch (column) {   //check a every colum separate by a coma
-                case'0':
-                    printf("ID :");
-                    break;
-                case'1':
-                    printf("ID_father :");
-                    break;
-                case'2':
-                    printf("ID_mother :");
-                    break;
-                case'3':
-                    printf("name :");
-                    break;
-                case'4':
-                    printf("surname :");
-                    break;
-                case'5':
-                    printf("surname :");
-                    break;
-                case'6':
-                    printf("Date :")
-                    break;
-                case'7':
-                    printf("Region :");
-                    break;
+    }
 
-
-            }
-            printf("%s", value);
-            value = strtok(NULL, ", ");
-            column++;
-        }
-        }
-    fclose(file);
-
+    return NULL;
 }
+
---
+++ file_reader.h
@@ -0,0 +1,8 @@
+
+
+#ifndef PROJET_FILE_READER_H
+#define PROJET_FILE_READER_H
+
+void read(char *nameFile);
+
+#endif 
