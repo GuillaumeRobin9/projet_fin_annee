2460a07041ad2678dbba1dc7a5c1d6f729bf3473
Mickaeln44 <mickael.neroda@gmail.com>
2021-06-26 23:35:38 +0200

    :rocket: creation HTML file

--- dataBase_reader.c
+++ dataBase_reader.c
@@ -7,17 +7,10 @@
 
 #include "dataBase_reader.h"
 
-void getAncestorsID(int IDchild, struct dataBase* data, int* IDfather, int* IDmother, int* IDPaternalGFather,int* IDPaternalGMother,int* IDMaternalGFather,int* IDMaternalGMother){
+int createPersonHTMLFile(){
 
-//    getting the IDs of the child's Ancestors
-    *IDfather = getFatherID(getPersonArray(data)[IDchild]);
-    *IDmother = getMotherID(getPersonArray(data)[IDchild]);
 
-//    father's parents
-    *IDPaternalGFather = getFatherID(getPersonArray(data)[*IDfather]);
-    *IDPaternalGMother = getMotherID(getPersonArray(data)[*IDfather]);
 
-//    mother's parents
-    *IDMaternalGFather = getFatherID(getPersonArray(data)[*IDmother]);
-    *IDMaternalGMother = getMotherID(getPersonArray(data)[*IDmother]);
 }
+
+
--- dataBase_reader.h
+++ dataBase_reader.h
@@ -1,12 +1,10 @@
-//
-// Created by micka on 26/06/2021.
-//
 
 #ifndef CODE_DATABASE_READER_H
 #define CODE_DATABASE_READER_H
 
 #include "structDataBase.h"
 
-void getAncestorsID(int IDchild, struct dataBase* data, int* IDfather, int* IDmother, int* IDPaternalGFather,int* IDPaternalGMother,int* IDMaternalGFather,int* IDMaternalGMother);
+int createPersonHTMLFile();
+
 
 #endif //CODE_DATABASE_READER_H
