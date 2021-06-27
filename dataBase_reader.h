d4025fbfd27633bffdf5bd5539121bc13c478ad2
Mickaeln44 <mickael.neroda@gmail.com>
2021-06-27 22:29:31 +0200

    :rocket: improving

--- dataBase_reader.h
+++ dataBase_reader.h
@@ -6,5 +6,14 @@
 
 void createPersonHTMLFile(struct Person* child, struct Person* father, struct Person* mother, struct Person* PaternalGFather, struct Person* PaternalGMother, struct Person* MaternalGFather, struct Person* MaternalGMother);
 
+// query Result File creation
+void createQueryResFile();
+
+// query Result File filling
+void fillFileQuery1(int ID, struct dataBase* data);
+void fillFileQuery2(int ID, struct dataBase* data);
+void fillFileQuery3(char* regionName, int numberPersonBorn);
+void fillFileQuery4(char* regionName);
+void fillFileQuery5(int day, int month, int numberPerson);
 
 #endif //CODE_DATABASE_READER_H
