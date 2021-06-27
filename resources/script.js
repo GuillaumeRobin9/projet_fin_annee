c5de525791f0c5b6dbc243ab840a0be06ca185ab
Antoine <antoine.sdmir@gmail.com>
2021-06-27 23:15:09 +0200

    :recycle: update JS ans CSS

--- resources/script.js
+++ resources/script.js
@@ -4,7 +4,6 @@ function unknownPerson(){
     const birth = document.getElementsByClassName("birthDay");
     const region = document.getElementsByClassName("region");
     const links = document.querySelectorAll("a");
-    console.log(links);
 
     for (let index = 0; index < personID.length; index++) {
         if (personID[index].innerHTML === "Name : -") {
@@ -21,6 +20,56 @@ function unknownPerson(){
 
 
 
+}
+
+function getRegionInformations(region, location, latitude, longitude){
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", `https://api.openweathermap.org/data/2.5/weather?q=${region}&appid=840322c4444cb74f0403b138879badea`);
+    xhr.onload = function () {
+        switch (xhr.status){
+        case 200:
+            parseData(JSON.parse(xhr.responseText), location, latitude, longitude);
+            break;
+        default:
+            console.log("FAILED AT PARSING DATA");
+            parseData(JSON.parse(xhr.responseText), location, latitude, longitude);
+        }
+    }
+    xhr.send();
+
+}
+
+function parseData(data, location, latitude, longitude){
+    //** Updating Values */
+    console.log(data["cod"]);
+    if (data["cod"] === "404") {
+        console.log("ENTERED HERE");
+        location.innerHTML = `Not Found..`;
+        latitude.innerHTML = `Not Found..`;
+        longitude.innerHTML = `Not Found..`;
+    } else{
+        location.innerHTML = `Located in : ${data["sys"]["country"]}`;
+        latitude.innerHTML = `Latitude is : ${data["coord"]["lat"]}`;
+        longitude.innerHTML = `Longitude is : ${data["coord"]["lon"]}`;
+    }
+
+}
+
+function clicked(id){
+
+    //** Parsing Bubble Inputs */
+    let fatherInfos = document.getElementById(id);
+    console.log(fatherInfos);
+    let infobulle = fatherInfos.getElementsByClassName("infobulle");
+    const region = fatherInfos.getElementsByClassName("region")[0].innerHTML
+    console.log();
+    const location = infobulle[0].children[0];
+    const latitude = infobulle[0].children[1];
+    const longitude = infobulle[0].children[2];
+    
+    //** INFO BUBBLE UPDATES */
+    getRegionInformations(region.substring(8,), location, latitude, longitude);
 }
 
 unknownPerson()
